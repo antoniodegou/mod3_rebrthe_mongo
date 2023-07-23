b5fd5e4c98f2e047525a407d8612cfa28449f719
antonio de gou <thestockofantonio@gmail.com>
2023-07-23 10:50:54 +0100

    reviewed exercises, exercises user and dashboard files

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,15 +1,18 @@
-let toastElList = [].slice.call(document.querySelectorAll('.toast'));
-let toastList = toastElList.map(function (toastEl) {
+// Get all toast elements on the page
+const toastElList = [].slice.call(document.querySelectorAll('.toast'));
+
+// Create an array of Bootstrap Toast instances for each toast element
+const toastList = toastElList.map(function (toastEl) {
     return new bootstrap.Toast(toastEl);
 });
+
+// Show each toast on the page
 toastList.forEach(function (toast) {
     toast.show();
 });
 
-
 // Get the current year
 const currentYear = new Date().getFullYear();
-    
+
 // Update the copyright year in the footer
 document.getElementById("currentYear").innerHTML = `&copy; ${currentYear} reBrthe, Inc`;
-
